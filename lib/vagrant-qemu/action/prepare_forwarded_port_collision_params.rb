11988ce1a377afdec40e081cbf97dd2a2ef19a29
gaozengqi <gaozengqi@hashdata.cn>
2022-05-05 10:26:51 +0800

    Remap forwarded_port "ssh" to ssh_port to fix port collision problem

--- lib/vagrant-qemu/action/prepare_forwarded_port_collision_params.rb
+++ lib/vagrant-qemu/action/prepare_forwarded_port_collision_params.rb
@@ -1,26 +1,34 @@
 module VagrantPlugins
-	module QEMU
-	  module Action
-		class PrepareForwardedPortCollisionParams
-		  def initialize(app, env)
-			@app = app
-		  end
-  
-		  def call(env)
-			machine = env[:machine]
-  
-			# TODO: not supported
-			other_used_ports = {}
-			env[:port_collision_extra_in_use] = other_used_ports
-  
-			# Build the remap for any existing collision detections
-			remap = {}
-			env[:port_collision_remap] = remap
-  
-			@app.call(env)
-		  end
-		end
-	  end
-	end
+  module QEMU
+    module Action
+    class PrepareForwardedPortCollisionParams
+      def initialize(app, env)
+      @app = app
+      end
+
+      def call(env)
+      machine = env[:machine]
+
+      # TODO: not supported
+      other_used_ports = {}
+      env[:port_collision_extra_in_use] = other_used_ports
+
+      # Build the remap for any existing collision detections
+      remap = {}
+      env[:port_collision_remap] = remap
+      machine.config.vm.networks.each do |type, options|
+        next if type != :forwarded_port
+
+        # remap ssh.host to ssh_port
+        if options[:id] == "ssh"
+          remap[options[:host]] = machine.provider_config.ssh_port
+          break
+        end
+      end
+
+      @app.call(env)
+      end
+    end
+    end
   end
-  
+end
